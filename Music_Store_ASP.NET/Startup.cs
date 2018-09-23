7da0a8488637344360e40118f795bd9c0cd23178
tim <timothymonague@yahoo.ca>
2018-09-23 02:48:11 -0400

    lesson 1. app setup

---
+++ Music_Store_ASP.NET/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Music_Store_ASP.NET.Startup))]
+namespace Music_Store_ASP.NET
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
